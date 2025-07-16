95bdd8b7c8786972ec234eb9471c4e9dbc95706d
bbatiste05 <bbatiste07@gmail.com>
2025-07-15 20:48:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 from load_data import get_dataset
 from dashboards import cost_anomalies, fraud_detection, risk_scoring, cpt_charge_audit, no_show_model
 
-
-
 st.set_page_config(page_title="Healthcare Claims Dashboard", layout="wide")
 
 st.title("🏥 Healthcare Claims Dashboard")
@@ -15,17 +13,21 @@ tab = st.selectbox("Choose a dashboard:", [
     "💰 Cost Anomalies",
     "🕵️ Fraud Detection",
     "💥 CPT Charge Audit",
-    "🤖 No-Show Predictor"  # New tab
+    "🤖 No-Show Predictor"
 ])
 
-with tab == "🧍 Risk Scoring":
+if tab == "🧍 Risk Scoring":
     risk_scoring.run(claims_df)
-with tab == "💰 Cost Anomalies":
+
+elif tab == "💰 Cost Anomalies":
     cost_anomalies.run(claims_df)
-with tab == "🕵️ Fraud Detection":
+
+elif tab == "🕵️ Fraud Detection":
     fraud_detection.run(claims_df)
-with tab == "💥 CPT Charge Audit":
+
+elif tab == "💥 CPT Charge Audit":
     cpt_charge_audit.run(claims_df)
-with tab == "🤖 No-Show Predictor":
-    no_show_model.run() 
+
+elif tab == "🤖 No-Show Predictor":
+    no_show_model.run()  # Allow file upload here
 
