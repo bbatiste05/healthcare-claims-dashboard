e7f6c5199e1e25a3afb8da59e8eb05ee258d3752
Brandon Batiste <bbatiste07@gmail.com>
2025-10-05 18:43:00 -0500

    Update __init__.py

--- copilot/__init__.py
+++ copilot/__init__.py
@@ -1,1 +1,7 @@
+# make copilot importable on Streamlit Cloud
+import sys, os
+pkg_root = os.path.dirname(__file__)
+parent = os.path.dirname(pkg_root)
+if parent not in sys.path:
+    sys.path.append(parent)
 
